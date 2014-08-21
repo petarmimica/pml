aa0764130941a552767f50f842b842bcf42a8113
Petar Mimica <petar.mimica@gmail.com>
2014-08-21 17:04:16 +0200

    Small fixes

--- project.R
+++ project.R
@@ -146,4 +146,13 @@ outname=paste(nameid,"rf",1,"Rdata",sep=".")
 load(outname)
 
 finaltestPred <- predict(tmpfit, newdata=testtotal)
-print(finaltestPred)
+
+pml_write_files = function(x){
+    n = length(x)
+    for(i in 1:n){
+        filename = paste0("problem_id_",i,".txt")
+        write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+    }
+}
+
+pml_write_files(finaltestPred)
